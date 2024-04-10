5d4eaa24b56ac9cc0e0d37f5bdaffaa37e45de02
vascsx <vasczn@gmail.com>
2024-04-10 14:36:21 -0300

    feat: create initial structure html from page login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { EditarComponent } from './pages/editar/editar.component';
+import { LoginComponent } from './pages/login/login.component';
+// import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 
 export const routes: Routes = [
     {
@@ -17,5 +19,15 @@ export const routes: Routes = [
     {
         path: 'editar/:id',
         component: EditarComponent
+    },
+
+    // {
+    //     path: 'detalhes/:id',
+    //     component: DetalhesComponent
+    // }
+
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
--- src/app/pages/detalhes/detalhes.component.ts
+++ src/app/pages/detalhes/detalhes.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { FuncionarioService } from '../../services/funcionario.service';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Funcionario } from '../../models/Funcionarios';
 
-@Component({
-  selector: 'app-detalhes',
-  standalone: true,
-  imports: [],
-  templateUrl: './detalhes.component.html',
-  styleUrl: './detalhes.component.scss'
-})
-export class DetalhesComponent {
+// @Component({
+//   selector: 'app-detalhes',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './detalhes.component.html',
+//   styleUrl: './detalhes.component.scss'
+// })
+// export class DetalhesComponent implements OnInit{
 
-}
+//   // funcionario? : Funcionario;
+//   // constructor( private funcionarioService: FuncionarioService, private route: ActivatedRoute, private router: Router) {
+//   // }
+//   // ngOnInit(): void {
+//   //     const id = Number(this.route.snapshot.paramMap.get('id'));
+//   //     this.funcionarioService.getFuncionarios(id).subscribe((data) => {
+//   //     this.funcionario = data.dados;
+//   //   })
+
+// }
+  
+// }
